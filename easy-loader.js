1b108ccdb5e5a627b98b25454f52368ba1f401dc
rektide <rektide@voodoowarez.com>
2013-09-14 19:54:09 -0400

    Restructure code-base. Introduce "gouranga" multi-killer utility.

--- easy-loader.js
+++ easy-loader.js
@@ -1,22 +1,45 @@
-// STATE
-
-var hippieKill= true
-function stopHippieKilling(){
-	hippieKill= false
-}
-
-// GO
-
-chrome.tabs.onCreated.addListener(function(tab) {
-	chrome.experimental.processes.getProcessIdForTab(tab.id, function (processId) {
+/** kill one tab */
+function killTab(tab){
+	chrome.processes.getProcessIdForTab(tab.id, function (processId) {
 		if (!isNaN(parseFloat(processId)) && isFinite(processId)) {
 			chrome.experimental.processes.terminate(processId)
 		}
 	})
-})
+}
+
+/** start killing new tabs */
+function startKilling(){
+	chrome.tabs.onCreated.addListener(killTab)
+}
+
+/** stop killing new tabs */
+function stopKilling(){
+	chrome.tabs.onCreated.removeListener(killTab)
+}
+
+/** kill everything underneath the passed in, or everything if nothing was passed
+  @param winOrTab an array of Windows or Tabs, a Window, or a Tab. Whatever processes hosting the winOrTab target are killed.
+*/
+function gouranga(winOrTab){
+	if(!winOrTab){
+		chrome.windows.getAll({populate:true}, gouranga)
+	}else if(winOrTab instanceof Tab){
+		killTab(winOrTab)
+	}else if(winOrTab instanceof Window){
+		if(!winOrTab.tabs)
+			throw "No Tabs were populated"
+		winOrTab.tabs.forEach(gouranga)
+	}else if(winOrTab instanceof Array){
+		winOrTab.forEach(gouranga)
+	}
+}
+
+// GO
+
+startKilling()
 
 // UNTIL
 
 setTimeout(function(){
-	stopHippieKilling()
+	stopKilling()
 }, 5000)
